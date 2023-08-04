8fd175c8aaeaa4ae86750f0a16f692497473bc9c
sinedied <noda@free.fr>
2023-08-04 11:26:49 +0200

    ci: fix deployment

--- .azure/deploy.sh
+++ .azure/deploy.sh
@@ -21,13 +21,27 @@ docker image tag search-api "$REGISTRY_NAME.azurecr.io/search-api:$commit_sha"
 docker image push "$REGISTRY_SERVER/search-api:$commit_sha"
 
 az containerapp secret set \
-  --name "${CONTAINER_APP_NAMES[1]}" \
+  --name "${CONTAINER_APP_NAMES[0]}" \
   --resource-group "$RESOURCE_GROUP_NAME" \
   --secrets \
-    ACS_API_KEY="$CONTENT_SAFETY_API_KEY" \
-    OPENAI_API_KEY="$OPENAI_API_KEY" \
-    REDIS_KEY="$REDIS_KEY" \
-  --output tsv
+    "acs-api-key=$CONTENT_SAFETY_API_KEY" \
+    "openai-api-key=$OPENAI_API_KEY" \
+    "redis-key=$REDIS_KEY" \
+  --output none
+
+# revision_name=$(
+#   az containerapp revision list \
+#     --name "${CONTAINER_APP_NAMES[0]}" \
+#     --resource-group "$RESOURCE_GROUP_NAME" \
+#     --query "[0].name" \
+#     --output tsv
+# )
+
+# # Container app needs to be restarted to pick up new secrets
+# az containerapp revision restart \
+#   --revision "$revision_name" \
+#   --resource-group "$RESOURCE_GROUP_NAME" \
+#   --output none
 
 az containerapp update \
   --name "${CONTAINER_APP_NAMES[0]}" \
@@ -41,14 +55,14 @@ az containerapp update \
     QD_PORT="80" \
     REDIS_HOST="${REDIS_HOSTNAME}" \
     ACS_API_URL="${CONTENT_SAFETY_ENDPOINT}" \
-    ACS_API_KEY="secretref:CONTENT_SAFETY_API_KEY" \
+    ACS_API_KEY="secretref:acs-api-key" \
     OPENAI_ADA_DEPLOY_ID="${OPENAI_MODEL_NAMES[0]}" \
     OPENAI_GPT_DEPLOY_ID="${OPENAI_MODEL_NAMES[1]}" \
     OPENAI_API_URL="${OPENAI_ENDPOINT}" \
-    OPENAI_API_KEY="secretref:OPENAI_API_KEY" \
-    REDIS_KEY="secretref:REDIS_KEY" \
+    OPENAI_API_KEY="secretref:openai-api-key" \
+    REDIS_KEY="secretref:redis-key" \
   --query "properties.configuration.ingress.fqdn" \
-  --output tsv
+  --output none
 
 echo "Deploying website..."
 cd packages/search-ui
